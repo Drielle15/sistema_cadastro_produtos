2cb0f559ab020f9d27db29e14b1b5b7e2be83b56
Drielle Dantas <drielle.dantas@aluno.senai.br>
2026-08-03 14:37:19 -0300

    Terminado

--- src/app/produtos/produto.ts
+++ src/app/produtos/produto.ts
@@ -1,5 +1,13 @@
+// Interface que define como deve ser um produto
+// Ou seja, todo produto precisa ter esses três campos
 export interface Produto {
+
+  // Identificador único do produto
     id: number;
+
+  // Nome ou descrição do produto
     descricao: string;
+
+  // Preço do produto
     valor: number;
   }
--- src/app/produtos/produtos.ts
+++ src/app/produtos/produtos.ts
@@ -2,39 +2,41 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Produto } from './produto';
 @Component({
-  selector: 'app-produtos',
-  imports: [FormsModule],
-  templateUrl: './produtos.html',
-  styleUrl: './produtos.css',
+  selector: 'app-produtos', // Nome da tag do componente
+  imports: [FormsModule], // Importa o módulo de formulários
+  templateUrl: './produtos.html', // Arquivo HTML da tela
+  styleUrl: './produtos.css', // Arquivo de estilos (CSS)
 })
 export class Produtos {
 
-  id: number = 1;
-  descricao: string = '';
-  valor: number = 0;
-
+  id: number = 1; //ID inicial do primeiro produto
+  descricao: string = '';  // Guarda a descrição digitada
+  valor: number = 0; // Guarda o valor digitado
+   // Lista onde os produtos serão armazenados
   produtos: Produto[] = [];
 
 
   adicionarProduto() {
-
+     // Cria um novo objeto com os dados informados pelo usuário
     const produto: Produto = {
       id: this.id,
       descricao: this.descricao,
       valor: this.valor
     };
 
+    // Adiciona o produto na lista
     this.produtos.push(produto);
-
+    // Aumenta o ID para o próximo produto
     this.id++;
 
+    // Limpa os campos do formulário
     this.limpar();
 
   }
 
 
   excluirProduto(id: number) {
-
+     // Remove da lista o produto que possui o ID recebido
     this.produtos = this.produtos.filter(
       produto => produto.id !== id
     );
@@ -44,7 +46,10 @@ export class Produtos {
 
   limpar() {
 
+     // Limpa a descrição
     this.descricao = '';
+
+     // Volta o valor para 0
     this.valor = 0;
 
   }
